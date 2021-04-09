6d42bc0d79d0ec4eecd0c26bf21861180135fd9e
avyrie <pittman.ally@gmail.com>
2021-04-08 20:35:47 -0600

    basic layout, can add todos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function Todo({ todo, index }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo">
+      { todo.text }
     </div>
-  );
+  )
+}
+
+function TodoForm({addTodo}) {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  }
+
+  return(
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Add Todo" className="input" value={value} onChange={e => setValue(e.target.value)} />
+    </form>
+  )
+}
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: 'Learn about react',
+      isCompleted: false
+    },
+    {
+      text: 'eat an apple',
+      isCompleted: false
+    },
+    {
+      text: 'apply for jobs',
+      isCompleted: false
+    },
+    {
+      text: 'go for a walk',
+      isCompleted: false
+    },
+    {
+      text: 'pet the cat',
+      isCompleted: false
+    },
+    {
+      text: 'clean the litterbox',
+      isCompleted: false
+    },
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+
+    </div>
+  )
 }
 
 export default App;
